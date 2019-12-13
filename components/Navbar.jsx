b5987fb1780e69faae040d07f7e6cb1438931705
Simon <warriv93@gmail.com>
2019-12-13 14:28:04 +0100

    initial commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+
+const Navbar = () => (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="collapse navbar-collapse" id="navbarColor01">
+            <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                <a className="nav-link" href="/">Home</a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link" href="/bitzPrices">Bitcoin prices</a>
+                </li>
+            </ul>
+          
+        </div>
+    </nav> 
+);
+  
+export default Navbar;
